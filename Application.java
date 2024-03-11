629b035d0ad0bcdce06afa1e8eafce501f2b107d
ethanfields923 <151499072+ethanfields923@users.noreply.github.com>
2024-03-11 14:56:39 -0400

    Commit application.java

---
+++ Application.java
@@ -0,0 +1,16 @@
+void getAllStudents(){
+
+}
+
+boolean addStudent(string first_name, string last_name, string email, string enrollment_date){
+
+
+}
+
+boolean updateStudentEmail(int student_id, string new_email){
+
+}
+
+boolean deleteStudent(int student_id){
+    
+}
